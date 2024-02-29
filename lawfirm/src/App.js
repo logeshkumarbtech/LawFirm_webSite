3133205fc43ba20a0d2e0bb84d9d2555be6907d7
Logesh kumar G <logeshkumarbtech@gmail.com>
2024-02-29 23:19:10 +0530

    grid section

--- lawfirm/src/App.js
+++ lawfirm/src/App.js
@@ -1,11 +1,13 @@
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
+import Practices from "./Components/Practices/Practices";
 
 function App() {
   return (
     <>
       <Home/>
       <About/>
+      <Practices/>
     </>
   );
 }
